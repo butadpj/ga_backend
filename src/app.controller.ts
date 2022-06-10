b81d178c23c0858a50d04edf0a15f7f9aa0c42a4
Paul John <butadpj@gmail.com>
2022-06-10 11:09:25 +0800

    Remove unused service in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,12 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('protected')
-  hello(@Request() req: any): string {
-    return req.user;
+  getHome(): string {
+    return this.appService.getHome();
   }
 }
